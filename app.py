0bb53ccbda7d058bdebc39d4c3d776ffda649db7
tushar-1920 <your-email@example.com>
2026-03-23 18:10:34 +0530

    Files changed and pushed to the github

--- app.py
+++ app.py
@@ -339,11 +339,13 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
-
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
